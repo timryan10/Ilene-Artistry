81dabddd9ddd6133df6e90127f814a47a3e50ab9
Tim <rtim10@ymail.com>
2025-06-09 21:36:15 -0400

    added modal

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,6 +1,11 @@
 import './index.css';
+import { Description, Dialog, DialogPanel, DialogTitle } from '@headlessui/react';
+import { useState } from 'react';
 
 function Header() {
+
+    let [isOpen, setIsOpen] = useState(false)
+
     return(
         <div id='Header'>
             <div id='logo'>
@@ -10,7 +15,18 @@ function Header() {
                 <h3>Portfolio</h3>
             </div>
             <div>
-                <h3>Make A Request</h3>
+                <button onClick={() => setIsOpen(true)}>Make A Request</button>
+                <Dialog open={isOpen} onClose={() => setIsOpen(false)} className="relative z-50">
+                    <div className='fixed inset-0 flex w-screen items-center justify-center p-4'>
+                        <DialogPanel className="max-w-lg space-y-4 border bg-white p-12">
+                            <DialogTitle className="font-bold">Make A Request</DialogTitle>
+                            <Description>If you would like a personalized drawing, please put in the requested information in the boxes below. Please add images that you would like to use for reference as well.</Description>
+                            <div className='flex gap-4'>
+                                <button onClick={() => setIsOpen(false)}>Cancel</button>
+                            </div>
+                        </DialogPanel>
+                    </div>
+                </Dialog>
             </div>
             <div>
                 <h3>About The Artist</h3>
